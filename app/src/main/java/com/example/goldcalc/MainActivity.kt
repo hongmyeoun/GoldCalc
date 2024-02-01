677708f6f81c0fbd2f787585131267af23c18106
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-02-01 23:43:30 +0900

    feat : Navigation 기초 코드 적어두기

--- app/src/main/java/com/example/goldcalc/MainActivity.kt
+++ app/src/main/java/com/example/goldcalc/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.goldcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.goldcalc.ui.theme.GoldCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +17,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GoldCalcTheme {
-
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "main"){
+                    composable("main"){
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "No Data")
+                        }
+                    }
+                }
             }
         }
     }
