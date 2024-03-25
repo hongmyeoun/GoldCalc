0dd60b763297274ed1e60bf94c617c00c0be49fa
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-25 20:57:25 +0900

    feat : CharacterCard 틀
    나중에 이미지를 다 첨부하기

--- app/src/main/java/com/example/goldcalc/MainActivity.kt
+++ app/src/main/java/com/example/goldcalc/MainActivity.kt
@@ -3,16 +3,43 @@ package com.example.goldcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.goldcalc.ui.theme.GoldCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,41 +47,91 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GoldCalcTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "main"){
-                    composable("main"){
-                        Column(
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Row {
-                                CharacterCard("캐릭터1")
-                                CharacterCard("캐릭터2")
-                            }
-                            Row {
-                                CharacterCard("캐릭터3")
-                                CharacterCard("캐릭터4")
-                            }
-                            Row {
-                                CharacterCard("캐릭터5")
-                                CharacterCard("캐릭터6")
-                            }
-                        }
-                    }
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun CharacterCard(character: String){
-    Card {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+fun MainScreen() {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        item { Character() }
+    }
+}
+
+@Composable
+fun Character() {
+    var progressPercentage by remember { mutableStateOf(0.0f) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = character)
+            Icon(
+                modifier = Modifier.weight(0.5f),
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "직업"
+            )
+            Column(
+                modifier = Modifier.weight(2f)
+            ) {
+                Text(text = "서버")
+                Row {
+                    Text(text = "닉네임")
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "골드체크")
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "설정")
+                }
+                Row {
+                    Text(text = "레벨")
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = "직업")
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text(text = "진행도")
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = "${(progressPercentage*100).toInt()}%")
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                LinearProgressIndicator(
+                    progress = progressPercentage,
+                    color = Color.Green
+                )
+            }
+        }
+        Row {
+            OutlinedButton(onClick = { progressPercentage += 0.1f }, enabled = progressPercentage < 1f) {
+                Text(text = "군단장 레이드")
+            }
+            OutlinedButton(onClick = { progressPercentage += 0.1f }, enabled = progressPercentage < 1f) {
+                Text(text = "어비스 던전")
+            }
+        }
+        Row {
+            OutlinedButton(onClick = { progressPercentage += 0.1f }, enabled = progressPercentage < 1f) {
+                Text(text = "어비스 레이드")
+            }
+            OutlinedButton(onClick = { progressPercentage = 0.0f }) {
+                Text(text = "0%")
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharacterPreview(){
+    MainScreen()
 }
