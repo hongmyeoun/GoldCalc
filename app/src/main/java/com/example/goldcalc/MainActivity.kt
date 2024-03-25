5e77181b8e26f294c69dfe1b438a1dfed5afdacb
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-25 23:49:43 +0900

    feat : Setting 틀(2관문 보스만)
    phase
    각 보스의 관문별 클리어, 더보기 골드를 반환
    
    twoPhaseBoss
    두 관문의 총 골드를 반환하는 함수
    
    Setting
    - Card를 누르면 세로가 길어지면서 컨텐츠가 나오게됨
    - 클릭을 하면서 골드량을 실시간으로 확인할 수 있음
    bug
    - expanded가 두번 바뀌면 if 문 내부 값이 초기화 됨...
    - 이를 해결하기 위해 db를 이용하던 shared를 이용하든 해야할 듯

--- app/src/main/java/com/example/goldcalc/MainActivity.kt
+++ app/src/main/java/com/example/goldcalc/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,11 +19,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
@@ -102,7 +102,7 @@ fun Character() {
                 ) {
                     Text(text = "진행도")
                     Spacer(modifier = Modifier.width(10.dp))
-                    Text(text = "${(progressPercentage*100).toInt()}%")
+                    Text(text = "${(progressPercentage * 100).toInt()}%")
                 }
                 Spacer(modifier = Modifier.height(10.dp))
                 LinearProgressIndicator(
@@ -130,8 +130,185 @@ fun Character() {
     }
 }
 
+@Composable
+fun Setting() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            var expanded by remember { mutableStateOf(true) }
+            var totalGold by remember { mutableStateOf(0) }
+            Card(
+                modifier = Modifier
+                    .animateContentSize()
+                    .height(if (expanded) 800.dp else 50.dp)
+                    .fillMaxWidth()
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) {
+                        expanded = !expanded
+                    }
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.weight(0.5f),
+                        imageVector = Icons.Default.AccountBox,
+                        contentDescription = "군단장레이드 이미지"
+                    )
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = "군단장"
+                    )
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "$totalGold")
+                    }
+                    Icon(
+                        modifier = Modifier.weight(0.5f),
+                        imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                        contentDescription = "펼치기"
+                    )
+                }
+                if (expanded) {
+                    val valtan = twoPhaseBoss(name = "발탄", smn1 = 300, smn2 = 400, ncg1 = 500, ncg2 = 700, smh1 = 450, smh2 = 600, hcg1 = 700, hcg2 = 1100)
+                    val biackiss = twoPhaseBoss(name = "비아키스", smn1 = 300, smn2 = 450, ncg1 = 600, ncg2 = 1000, smh1 = 500, smh2 = 650, hcg1 = 900, hcg2 = 1500)
+
+                    totalGold = valtan + biackiss
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun twoPhaseBoss(
+    name: String,
+    smn1: Int,
+    smn2: Int,
+    ncg1: Int,
+    ncg2: Int,
+    smh1: Int,
+    smh2: Int,
+    hcg1: Int,
+    hcg2: Int
+): Int {
+    var totalGold by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "보스 이미지")
+            Text(
+                modifier = Modifier.weight(3f),
+                text = name
+            )
+            Row(
+                modifier = Modifier.weight(1f),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "$totalGold")
+            }
+        }
+        val (cg1, smg1) = phase(gate = "1 관문", seeMoreGoldN = smn1, seeMoreGoldH = smh1, clearGoldN = ncg1, clearGoldH = hcg1)
+        val (cg2, smg2) = phase(gate = "2 관문", seeMoreGoldN = smn2, seeMoreGoldH = smh2, clearGoldN = ncg2, clearGoldH = hcg2)
+
+        totalGold = cg1 + cg2 - smg1 - smg2
+    }
+
+    return totalGold
+}
+
+@Composable
+fun phase(
+    gate: String,
+    seeMoreGoldN: Int,
+    seeMoreGoldH: Int,
+    clearGoldN: Int,
+    clearGoldH: Int
+): Pair<Int, Int> {
+    var level by remember { mutableStateOf("노말") }
+    var seeMoreCheck by remember { mutableStateOf(false) }
+    var clearCheck by remember { mutableStateOf(false) }
+    var seeMoreGold by remember { mutableStateOf(0) }
+    var clearGold by remember { mutableStateOf(0) }
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(3f)
+                    .padding(start = 16.dp),
+                text = gate
+            )
+            OutlinedButton(
+                onClick = {
+                    level = if (level == "노말") "하드" else "노말"
+                    seeMoreGold = if (seeMoreCheck && level == "노말") seeMoreGoldN else if (seeMoreCheck) seeMoreGoldH else 0
+                    clearGold = if (clearCheck && level == "노말") clearGoldN else if (clearCheck) clearGoldH else 0
+                }
+            ) {
+                Text(text = level)
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = clearCheck,
+                    onCheckedChange = {
+                        clearCheck = !clearCheck
+                        clearGold = if (clearCheck && level == "노말") clearGoldN else if (clearCheck && level == "하드") clearGoldH else 0
+                    }
+                )
+                Text(text = "클리어")
+            }
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = seeMoreCheck,
+                    onCheckedChange = {
+                        seeMoreCheck = !seeMoreCheck
+                        seeMoreGold = if (seeMoreCheck && level == "노말") seeMoreGoldN else if (seeMoreCheck && level == "하드") seeMoreGoldH else 0
+                    }
+                )
+                Text(text = "더보기")
+            }
+        }
+    }
+    return Pair(clearGold, seeMoreGold)
+}
+
 @Preview(showBackground = true)
 @Composable
-fun CharacterPreview(){
-    MainScreen()
+fun CharacterPreview() {
+    Setting()
 }
