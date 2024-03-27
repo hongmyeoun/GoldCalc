1ae90148faadfe5f4115a6aac3ebc8c422fb9b65
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-27 20:10:05 +0900

    ref : 군단장 더보기, 클골 및 정보를 model에서 관리
    변경전
    - view에서 직접 값을 사용
    
    변경후
    - view에서 model로 분리
    - 데이터를 model에서 vm으로 가져오고 vm에서 view로 전달하는 방식

---
+++ app/src/main/java/com/hongmyeoun/goldcalc/model/CommandBoss.kt
@@ -0,0 +1,106 @@
+package com.hongmyeoun.goldcalc.model
+
+enum class CommandBoss(
+    val boss: String,
+    val seeMoreGold: Map<String, List<Int>>,
+    val clearGold: Map<String, List<Int>>
+) {
+    VALTAN(
+        boss = "발탄",
+        seeMoreGold = mapOf(
+            "normal" to listOf(300, 400),
+            "hard" to listOf(450, 600)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(500, 700),
+            "hard" to listOf(700, 1100)
+        )
+    ),
+    BIACKISS(
+        boss = "비아키스",
+        seeMoreGold = mapOf(
+            "normal" to listOf(300, 450),
+            "hard" to listOf(500, 650)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(600, 1000),
+            "hard" to listOf(900, 1500)
+        )
+    ),
+    KOUKU_SATON(
+        boss = "쿠크세이튼",
+        seeMoreGold = mapOf(
+            "normal" to listOf(300, 500, 600),
+            "hard" to listOf(300, 500, 600)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(600, 900, 1500),
+            "hard" to listOf(600, 900, 1500)
+        )
+    ),
+    ABRELSHUD(
+        boss = "아브렐슈드",
+        seeMoreGold = mapOf(
+            "normal" to listOf(400, 600, 800, 1500),
+            "hard" to listOf(700, 900, 1100, 1800)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(1500, 1500, 1500, 2500),
+            "hard" to listOf(2000, 2000, 2000, 3000)
+        )
+    ),
+    ILLIAKAN(
+        boss = "일리아칸",
+        seeMoreGold = mapOf(
+            "normal" to listOf(900, 1100, 1500),
+            "hard" to listOf(1200, 1400, 1900)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(1500, 2000, 4000),
+            "hard" to listOf(1750, 2500, 5750)
+        )
+    ),
+    KAMEN(
+        boss = "카멘",
+        seeMoreGold = mapOf(
+            "normal" to listOf(1500, 1800, 2500, 0),
+            "hard" to listOf(2000, 2400, 2800, 3600)
+        ),
+        clearGold = mapOf(
+            "normal" to listOf(3500, 4000, 5500, 0),
+            "hard" to listOf(5000, 6000, 9000, 21000)
+        )
+    )
+}
+
+class CommandBossModel {
+    var valtan = CommandBoss.VALTAN.boss
+    var valtanSMG = CommandBoss.VALTAN.getBossInfo("normal").first + CommandBoss.VALTAN.getBossInfo("hard").first
+    var valtanCG =  CommandBoss.VALTAN.getBossInfo("normal").second + CommandBoss.VALTAN.getBossInfo("hard").second
+
+    var bi = CommandBoss.BIACKISS.boss
+    var biSMG = CommandBoss.BIACKISS.getBossInfo("normal").first + CommandBoss.BIACKISS.getBossInfo("hard").first
+    var biCG = CommandBoss.BIACKISS.getBossInfo("normal").second + CommandBoss.BIACKISS.getBossInfo("hard").second
+
+    var kuku = CommandBoss.KOUKU_SATON.boss
+    var kukuSMG = CommandBoss.KOUKU_SATON.getBossInfo("normal").first + CommandBoss.KOUKU_SATON.getBossInfo("hard").first
+    var kukuCG = CommandBoss.KOUKU_SATON.getBossInfo("normal").second + CommandBoss.KOUKU_SATON.getBossInfo("hard").second
+
+    var abrel = CommandBoss.ABRELSHUD.boss
+    var abrelSMG = CommandBoss.ABRELSHUD.getBossInfo("normal").first + CommandBoss.ABRELSHUD.getBossInfo("hard").first
+    var abrelCG = CommandBoss.ABRELSHUD.getBossInfo("normal").second + CommandBoss.ABRELSHUD.getBossInfo("hard").second
+
+    var illi = CommandBoss.ILLIAKAN.boss
+    var illiSMG = CommandBoss.ILLIAKAN.getBossInfo("normal").first + CommandBoss.ILLIAKAN.getBossInfo("hard").first
+    var illiCG = CommandBoss.ILLIAKAN.getBossInfo("normal").second + CommandBoss.ILLIAKAN.getBossInfo("hard").second
+
+    var kamen = CommandBoss.KAMEN.boss
+    var kamenSMG = CommandBoss.KAMEN.getBossInfo("normal").first + CommandBoss.KAMEN.getBossInfo("hard").first
+    var kamenCG = CommandBoss.KAMEN.getBossInfo("normal").second + CommandBoss.KAMEN.getBossInfo("hard").second
+
+    private fun CommandBoss.getBossInfo(mode: String): Pair<List<Int>, List<Int>> {
+        val seeMoreGold = seeMoreGold[mode] ?: emptyList()
+        val clearGold = clearGold[mode] ?: emptyList()
+        return Pair(seeMoreGold, clearGold)
+    }
+}
--- app/src/main/java/com/hongmyeoun/goldcalc/viewModel/CommandBossVM.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/viewModel/CommandBossVM.kt
@@ -4,16 +4,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.hongmyeoun.goldcalc.model.CommandBossModel
 
 class CommandBossVM: ViewModel() {
-    var expanded by mutableStateOf(false)
-    var totalGold by mutableStateOf(0)
+    val cbModel = CommandBossModel()
 
+    var expanded by mutableStateOf(false)
     fun expand(){
         expanded = !expanded
     }
 
+    var totalGold by mutableStateOf(0)
     fun sumGold(va: Int, bi: Int, ku: Int, a: Int, il: Int, ka: Int){
         totalGold = va + bi + ku + a + il + ka
     }
+
+    val valtan = cbModel.valtan
+    val valtanSMG = cbModel.valtanSMG
+    val valtanCG =  cbModel.valtanCG
+
+    val bi = cbModel.bi
+    val biSMG = cbModel.biSMG
+    val biCG = cbModel.biCG
+
+    var kuku = cbModel.kuku
+    var kukuSMG = cbModel.kukuSMG
+    var kukuCG = cbModel.kukuCG
+
+    var abrel = cbModel.abrel
+    var abrelSMG = cbModel.abrelSMG
+    var abrelCG = cbModel.abrelCG
+
+    var illi = cbModel.illi
+    var illiSMG = cbModel.illiSMG
+    var illiCG = cbModel.illiCG
+
+    var kamen = cbModel.kamen
+    var kamenSMG = cbModel.kamenSMG
+    var kamenCG = cbModel.kamenCG
 }
