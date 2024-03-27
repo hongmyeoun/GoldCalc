695cc5d2c53e1f3eb478bfe0b4682045180561b2
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-28 00:58:32 +0900

    feat : 레이드 종류를 나누는 카드를 하나의 함수로 통합
    변경전
    - 군단장, 어비스던전, 카제로스, 에픽 네개의 함수가 비슷하게 쓰였음
    - expanded가 항상 false값으로 갱신되지 않았음
    
    변경후
    - 이제 하나의 RaidSettingCard로 관리 가능
    - UI가 그려질때 viewModel을 불러오니까 초기값인 false로 초기화 시켜서 항상 닫혀있던 것이었다. 파라미터로 viewModel을 빼니깐 됨!

--- app/src/main/java/com/hongmyeoun/goldcalc/MainActivity.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/MainActivity.kt
@@ -3,6 +3,9 @@ package com.hongmyeoun.goldcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,16 +15,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -35,8 +43,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hongmyeoun.goldcalc.ui.theme.GoldCalcTheme
-import com.hongmyeoun.goldcalc.view.AbyssDungeonCard
+import com.hongmyeoun.goldcalc.view.AbyssDungeon
 import com.hongmyeoun.goldcalc.view.CommandBossCard
 import com.hongmyeoun.goldcalc.view.EpicRaidCard
 import com.hongmyeoun.goldcalc.view.KazerothRaidCard
@@ -135,9 +144,9 @@ fun Character() {
 }
 
 @Composable
-fun Setting() {
+fun Setting(adVM: AbyssDungeonVM = viewModel()) {
     val cbVM = CommandBossVM()
-    val adVM = AbyssDungeonVM()
+//    val adVM = AbyssDungeonVM()
     val kzVM = KazerothRaidVM()
     val epVM = EpicRaidVM()
 
@@ -148,10 +157,85 @@ fun Setting() {
             .verticalScroll(rememberScrollState())
     ) {
         CommandBossCard(viewModel = cbVM)
-        AbyssDungeonCard(viewModel = adVM)
+        RaidSettingCard(
+            raidType = "어비스 던전",
+            totalGold = adVM.totalGold,
+            expanded = adVM.expanded,
+            onRightButtonClicked = { adVM.expand() },
+            onBottomClicked = { adVM.expand() },
+        ) {
+            AbyssDungeon(viewModel = adVM)
+        }
         KazerothRaidCard(viewModel = kzVM)
         EpicRaidCard(viewModel = epVM)
     }
+}
+
+@Composable
+fun RaidSettingCard(
+    raidType: String,
+    totalGold: Int,
+    expanded: Boolean,
+    onRightButtonClicked: () -> Unit,
+    onBottomClicked: () -> Unit,
+    raidContent: @Composable () -> Unit
+) {
+    val height = if (expanded) Modifier.wrapContentHeight() else Modifier.height(80.dp)
+
+    Card(
+        modifier = Modifier
+            .animateContentSize(animationSpec = tween(durationMillis = 1000))
+            .then(height)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier.weight(0.5f),
+                imageVector = Icons.Default.AccountBox,
+                contentDescription = "$raidType 이미지"
+            )
+            Text(
+                modifier = Modifier.weight(1f),
+                text = raidType
+            )
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "$totalGold")
+            }
+            IconButton(
+                modifier = Modifier.weight(0.5f),
+                onClick = { onRightButtonClicked() },
+                interactionSource = remember { MutableInteractionSource() },
+            ) {
+                Icon(
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "펼치기"
+                )
+            }
+        }
+
+        raidContent()
+
+        IconButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { onBottomClicked() }
+        ) {
+            Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "접기")
+        }
+    }
+
+    Spacer(modifier = Modifier.height(16.dp))
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/hongmyeoun/goldcalc/view/AbyssDungeonView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/AbyssDungeonView.kt
@@ -1,99 +1,19 @@
 package com.hongmyeoun.goldcalc.view
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.hongmyeoun.goldcalc.viewModel.AbyssDungeonVM
 
 @Composable
-fun AbyssDungeonCard(viewModel: AbyssDungeonVM){
-    val height = if (viewModel.expanded) Modifier.wrapContentHeight() else Modifier.height(80.dp)
-
-    Card(
-        modifier = Modifier
-            .animateContentSize(animationSpec = tween(durationMillis = 1000))
-            .then(height)
-            .fillMaxWidth()
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                modifier = Modifier.weight(0.5f),
-                imageVector = Icons.Default.AccountBox,
-                contentDescription = "어비스던전 이미지"
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = "어비스던전"
-            )
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "${viewModel.totalGold}")
-            }
-            IconButton(
-                modifier = Modifier.weight(0.5f),
-                onClick = { viewModel.expand() },
-                interactionSource = remember { MutableInteractionSource() },
-            ) {
-                Icon(
-                    imageVector = if (viewModel.expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
-                    contentDescription = "펼치기"
-                )
-            }
-        }
-
-        val kayangel = threePhaseBoss(
-            name = viewModel.kayang,
-            seeMoreGold = viewModel.kayangSMG,
-            clearGold = viewModel.kayangCG
-        )
-        val ivoryTower = fourPhaseBoss(
-            name = viewModel.ivT,
-            seeMoreGold = viewModel.ivTSMG,
-            clearGold = viewModel.ivTCG
-        )
-
-        viewModel.sumGold(kayangel, ivoryTower)
-
-        IconButton(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { viewModel.expand() }
-        ) {
-            Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "접기")
-        }
-    }
-
-    Spacer(modifier = Modifier.height(16.dp))
-
+fun AbyssDungeon(viewModel: AbyssDungeonVM){
+    val kayangel = threePhaseBoss(
+        name = viewModel.kayang,
+        seeMoreGold = viewModel.kayangSMG,
+        clearGold = viewModel.kayangCG
+    )
+    val ivoryTower = fourPhaseBoss(
+        name = viewModel.ivT,
+        seeMoreGold = viewModel.ivTSMG,
+        clearGold = viewModel.ivTCG
+    )
+    viewModel.sumGold(kayangel, ivoryTower)
 }
