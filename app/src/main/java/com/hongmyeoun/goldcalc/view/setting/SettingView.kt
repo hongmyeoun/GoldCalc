60d64cc584c2f275a8b8b169f62e563fa3c503f5
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-31 23:18:43 +0900

    feat : Setting -> 추가 골드와 사용골드를 추가해 캐릭 총 골드 추가
    총 골드를 통해 값을 더한값 표시
    viewModel로 표현

--- app/src/main/java/com/hongmyeoun/goldcalc/view/setting/SettingView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/setting/SettingView.kt
@@ -1,14 +1,24 @@
 package com.hongmyeoun.goldcalc.view.setting
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hongmyeoun.goldcalc.view.setting.cardContent.AbyssDungeon
@@ -18,15 +28,19 @@ import com.hongmyeoun.goldcalc.view.setting.cardContent.KazerothRaid
 import com.hongmyeoun.goldcalc.viewModel.AbyssDungeonVM
 import com.hongmyeoun.goldcalc.viewModel.CommandBossVM
 import com.hongmyeoun.goldcalc.viewModel.EpicRaidVM
+import com.hongmyeoun.goldcalc.viewModel.GoldSettingVM
 import com.hongmyeoun.goldcalc.viewModel.KazerothRaidVM
 
 @Composable
 fun Setting(
+    viewModel: GoldSettingVM = viewModel(),
     cbVM: CommandBossVM = viewModel(),
     adVM: AbyssDungeonVM = viewModel(),
     kzVM: KazerothRaidVM = viewModel(),
     epVM: EpicRaidVM = viewModel()
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -68,6 +82,46 @@ fun Setting(
             onArrowClicked = { epVM.expand() }
         ) {
             EpicRaid(viewModel = epVM)
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = viewModel.plusGold,
+                onValueChange = { viewModel.plusGoldValue(it) },
+                label = { Text(text = "추가 골드") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide() }
+                )
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = viewModel.minusGold,
+                onValueChange = { viewModel.minusGoldValue(it) },
+                label = { Text(text = "사용 골드") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide() }
+                )
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Row {
+                Text(text = "주간 수입")
+                Text(text = "${viewModel.calcTotalGold(cbVM.totalGold, adVM.totalGold, kzVM.totalGold, epVM.totalGold)}")
+            }
         }
     }
 }
---
+++ app/src/main/java/com/hongmyeoun/goldcalc/viewModel/GoldSettingVM.kt
@@ -0,0 +1,28 @@
+package com.hongmyeoun.goldcalc.viewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class GoldSettingVM: ViewModel() {
+    var plusGold by mutableStateOf("0")
+    var minusGold by mutableStateOf("0")
+    private var totalGold by mutableStateOf(0)
+
+    fun plusGoldValue(newValue: String){
+        plusGold = newValue.filter { it.isDigit() }
+    }
+
+    fun minusGoldValue(newValue: String){
+        minusGold = newValue.filter { it.isDigit() }
+    }
+
+    fun calcTotalGold(cb: Int, ad: Int, kz: Int, ep: Int): Int {
+        val plusGoldInt = plusGold.toIntOrNull()?:0
+        val minusGoldInt = minusGold.toIntOrNull()?:0
+        totalGold = cb + ad + kz + ep + plusGoldInt - minusGoldInt
+        return totalGold
+    }
+
+}
