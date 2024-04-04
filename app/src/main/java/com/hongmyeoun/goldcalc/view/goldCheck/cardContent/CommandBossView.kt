2495d44110bae40e196394032850422ed8f61923
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-04 18:41:29 +0900

    ref : 나머지 레이드와 같이 받는 방식 변환 fix : 데이터 계산을 너무 많이하고, UI를 불러오는데 스레드 문제가 생기는 문제
    fix
    - 군단장 6개를 전부 보여주면 렉이 걸림
    - 체크를 통해 고른것만 보여줌
    
    bug
    - 군단장을 고른뒤 체크를 하고, 다시 체크를 해서 없애버리면 골드는 초기화가 되지 않지만, 클리어, 더보기 체크사항은 초기화됨

--- app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/cardContent/CommandBossView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/cardContent/CommandBossView.kt
@@ -1,47 +1,205 @@
 package com.hongmyeoun.goldcalc.view.goldCheck.cardContent
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import com.hongmyeoun.goldcalc.view.goldCheck.fourPhaseBoss
 import com.hongmyeoun.goldcalc.view.goldCheck.threePhaseBoss
 import com.hongmyeoun.goldcalc.view.goldCheck.twoPhaseBoss
 import com.hongmyeoun.goldcalc.viewModel.goldCheck.CommandBossVM
 
 @Composable
-fun CommandRaid(viewModel: CommandBossVM) {
-    val valtan = twoPhaseBoss(
-        name = viewModel.valtan,
-        seeMoreGold = viewModel.valtanSMG,
-        clearGold = viewModel.valtanCG
-    )
-    val biackiss = twoPhaseBoss(
-        name = viewModel.bi,
-        seeMoreGold = viewModel.biSMG,
-        clearGold = viewModel.biCG
-    )
-    val koukuSaton = threePhaseBoss(
-        name = viewModel.kuku,
-        seeMoreGold = viewModel.kukuSMG,
-        clearGold = viewModel.kukuCG
-    )
-    val abrelshud = fourPhaseBoss(
-        name = viewModel.abrel,
-        seeMoreGold = viewModel.abrelSMG,
-        clearGold = viewModel.abrelCG
-    )
-    val illiakan = threePhaseBoss(
-        name = viewModel.illi,
-        seeMoreGold = viewModel.illiSMG,
-        clearGold = viewModel.illiCG
-    )
-    val kamen = fourPhaseBoss(
-        name = viewModel.kamen,
-        seeMoreGold = viewModel.kamenSMG,
-        clearGold = viewModel.kamenCG
-    )
+fun CommandRaid(
+    viewModel: CommandBossVM,
+) {
 
-    LaunchedEffect(valtan, biackiss, koukuSaton, abrelshud, illiakan, kamen) {
-        viewModel.sumGold(valtan, biackiss, koukuSaton, abrelshud, illiakan, kamen)
+    LaunchedEffect(viewModel.valtanTG, viewModel.biakissTG, viewModel.koukuSatonTG, viewModel.abrelshudTG, viewModel.illiakanTG, viewModel.kamenTG) {
+        viewModel.sumGold()
+    }
+
+    var valtanCheck by remember { mutableStateOf(false) }
+    var biaCheck by remember { mutableStateOf(false) }
+    var koukuCheck by remember { mutableStateOf(false) }
+    var abreCheck by remember { mutableStateOf(false) }
+    var illiCheck by remember { mutableStateOf(false) }
+    var kamenCheck by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "발탄")
+            Checkbox(
+                checked = valtanCheck,
+                onCheckedChange = {
+                    valtanCheck = !valtanCheck
+                }
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "비아")
+            Checkbox(
+                checked = biaCheck,
+                onCheckedChange = {
+                    biaCheck = !biaCheck
+                }
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "쿠크")
+            Checkbox(
+                checked = koukuCheck,
+                onCheckedChange = {
+                    koukuCheck = !koukuCheck
+                }
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "아브")
+            Checkbox(
+                checked = abreCheck,
+                onCheckedChange = {
+                    abreCheck = !abreCheck
+                }
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "일리")
+            Checkbox(
+                checked = illiCheck,
+                onCheckedChange = {
+                    illiCheck = !illiCheck
+                }
+            )
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Text(text = "카멘")
+            Checkbox(
+                checked = kamenCheck,
+                onCheckedChange = {
+                    kamenCheck = !kamenCheck
+                }
+            )
+        }
+    }
+
+    if (valtanCheck) {
+        twoPhaseBoss(
+            name = viewModel.valtan,
+            seeMoreGold = viewModel.valtanSMG,
+            clearGold = viewModel.valtanCG,
+            totalGold = viewModel.valtanTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.valtanOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.valtanTwoPhase(updateTotal)
+            }
+        )
+    }
+
+    if (biaCheck) {
+        twoPhaseBoss(
+            name = viewModel.bi,
+            seeMoreGold = viewModel.biSMG,
+            clearGold = viewModel.biCG,
+            totalGold = viewModel.biakissTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.biakissOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.biakissTwoPhase(updateTotal)
+            }
+        )
+    }
+
+    if (koukuCheck) {
+        threePhaseBoss(
+            name = viewModel.kuku,
+            seeMoreGold = viewModel.kukuSMG,
+            clearGold = viewModel.kukuCG,
+            totalGold = viewModel.koukuSatonTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.koukuSatonOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.koukuSatontwoPhase(updateTotal)
+            },
+            onUpdateTotalGoldThreePhase = { updateTotal ->
+                viewModel.koukuSatonThreePhase(updateTotal)
+            }
+        )
+    }
+
+    if (abreCheck) {
+        fourPhaseBoss(
+            name = viewModel.abrel,
+            seeMoreGold = viewModel.abrelSMG,
+            clearGold = viewModel.abrelCG,
+            totalGold = viewModel.abrelshudTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.abrelshudOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.abrelshudTwoPhase(updateTotal)
+            },
+            onUpdateTotalGoldThreePhase = { updateTotal ->
+                viewModel.abrelshudThreePhase(updateTotal)
+            },
+            onUpdateTotalGoldFourPhase = { updateTotal ->
+                viewModel.abrelshudFourPhase(updateTotal)
+            }
+        )
+    }
+
+    if (illiCheck) {
+        threePhaseBoss(
+            name = viewModel.illi,
+            seeMoreGold = viewModel.illiSMG,
+            clearGold = viewModel.illiCG,
+            totalGold = viewModel.illiakanTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.illiakanOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.illiakantwoPhase(updateTotal)
+            },
+            onUpdateTotalGoldThreePhase = { updateTotal ->
+                viewModel.illiakanThreePhase(updateTotal)
+            }
+        )
+    }
+
+    if (kamenCheck) {
+        fourPhaseBoss(
+            name = viewModel.kamen,
+            seeMoreGold = viewModel.kamenSMG,
+            clearGold = viewModel.kamenCG,
+            totalGold = viewModel.kamenTG,
+            onUpdateTotalGoldOnePhase = { updateTotal ->
+                viewModel.kamenOnePhase(updateTotal)
+            },
+            onUpdateTotalGoldTwoPhase = { updateTotal ->
+                viewModel.kamentwoPhase(updateTotal)
+            },
+            onUpdateTotalGoldThreePhase = { updateTotal ->
+                viewModel.kamenThreePhase(updateTotal)
+            },
+            onUpdateTotalGoldFourPhase = { updateTotal ->
+                viewModel.kamenFourPhase(updateTotal)
+            }
+        )
     }
 
 }
--- app/src/main/java/com/hongmyeoun/goldcalc/viewModel/goldCheck/CommandBossVM.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/viewModel/goldCheck/CommandBossVM.kt
@@ -7,16 +7,132 @@ import androidx.lifecycle.ViewModel
 import com.hongmyeoun.goldcalc.model.goldCheck.CommandBossModel
 
 class CommandBossVM: ViewModel() {
-    val cbModel = CommandBossModel()
-
-    var expanded by mutableStateOf(false)
-    fun expand(){
-        expanded = !expanded
-    }
+    private val cbModel = CommandBossModel()
 
     var totalGold by mutableStateOf(0)
-    fun sumGold(va: Int, bi: Int, ku: Int, a: Int, il: Int, ka: Int){
-        totalGold = va + bi + ku + a + il + ka
+    fun sumGold(){
+        totalGold = valtanTG + biakissTG + koukuSatonTG + abrelshudTG + illiakanTG + kamenTG
+    }
+
+    var valtanTG by mutableStateOf(0)
+    private var valtanOne by mutableStateOf(0)
+    private var valtanTwo by mutableStateOf(0)
+    fun valtanOnePhase(update: Int) {
+        valtanOne = update
+        valtanTotal()
+    }
+    fun valtanTwoPhase(update: Int) {
+        valtanTwo = update
+        valtanTotal()
+    }
+    private fun valtanTotal(){
+        valtanTG = valtanOne + valtanTwo
+    }
+
+    var biakissTG by mutableStateOf(0)
+    private var biakissOne by mutableStateOf(0)
+    private var biakissTwo by mutableStateOf(0)
+    fun biakissOnePhase(update: Int) {
+        biakissOne = update
+        biakissTotal()
+    }
+    fun biakissTwoPhase(update: Int) {
+        biakissTwo = update
+        biakissTotal()
+    }
+    private fun biakissTotal() {
+        biakissTG = biakissOne + biakissTwo
+    }
+
+    var koukuSatonTG by mutableStateOf(0)
+    private var koukuSatonOne by mutableStateOf(0)
+    private var koukuSatonTwo by mutableStateOf(0)
+    private var koukuSatonThree by mutableStateOf(0)
+    fun koukuSatonOnePhase(update: Int) {
+        koukuSatonOne = update
+        koukuSatonTotal()
+    }
+    fun koukuSatontwoPhase(update: Int) {
+        koukuSatonTwo = update
+        koukuSatonTotal()
+    }
+    fun koukuSatonThreePhase(update: Int) {
+        koukuSatonThree = update
+        koukuSatonTotal()
+    }
+    private fun koukuSatonTotal() {
+        koukuSatonTG = koukuSatonOne + koukuSatonTwo + koukuSatonThree
+    }
+
+
+    var illiakanTG by mutableStateOf(0)
+    private var illiakanOne by mutableStateOf(0)
+    private var illiakanTwo by mutableStateOf(0)
+    private var illiakanThree by mutableStateOf(0)
+    fun illiakanOnePhase(update: Int) {
+        illiakanOne = update
+        illiakanTotal()
+    }
+    fun illiakantwoPhase(update: Int) {
+        illiakanTwo = update
+        illiakanTotal()
+    }
+    fun illiakanThreePhase(update: Int) {
+        illiakanThree = update
+        illiakanTotal()
+    }
+    fun illiakanTotal() {
+        illiakanTG = illiakanOne + illiakanTwo + illiakanThree
+    }
+
+    var abrelshudTG by mutableStateOf(0)
+    private var abrelshudOne by mutableStateOf(0)
+    private var abrelshudTwo by mutableStateOf(0)
+    private var abrelshudThree by mutableStateOf(0)
+    private var abrelshudFour by mutableStateOf(0)
+    fun abrelshudOnePhase(update: Int) {
+        abrelshudOne = update
+        abrelshudTotal()
+    }
+    fun abrelshudTwoPhase(update: Int) {
+        abrelshudTwo = update
+        abrelshudTotal()
+    }
+    fun abrelshudThreePhase(update: Int) {
+        abrelshudThree = update
+        abrelshudTotal()
+    }
+    fun abrelshudFourPhase(update: Int) {
+        abrelshudFour = update
+        abrelshudTotal()
+    }
+    fun abrelshudTotal() {
+        abrelshudTG = abrelshudOne + abrelshudTwo + abrelshudThree + abrelshudFour
+    }
+
+    var kamenTG by mutableStateOf(0)
+    private var kamenOne by mutableStateOf(0)
+    private var kamenTwo by mutableStateOf(0)
+    private var kamenThree by mutableStateOf(0)
+    private var kamenFour by mutableStateOf(0)
+    fun kamenOnePhase(update: Int) {
+        kamenOne = update
+        kamenTotal()
+    }
+    fun kamentwoPhase(update: Int) {
+        kamenTwo = update
+        kamenTotal()
+    }
+    fun kamenThreePhase(update: Int) {
+        kamenThree = update
+        kamenTotal()
+    }
+    fun kamenFourPhase(update: Int) {
+        kamenFour = update
+        kamenTotal()
+    }
+    fun kamenTotal() {
+        kamenTG = kamenOne + kamenTwo + kamenThree + kamenFour
     }
 
     val valtan = cbModel.valtan
@@ -42,4 +158,5 @@ class CommandBossVM: ViewModel() {
     var kamen = cbModel.kamen
     var kamenSMG = cbModel.kamenSMG
     var kamenCG = cbModel.kamenCG
+
 }
