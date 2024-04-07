b7bc69feb9b61863b6e2dcca2444396ad900c312
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-07 13:41:49 +0900

    feat : KazerothRaidModel, Echidna
    Echidna
    에키드나 레이드를 관리하는 객체
    - 이름으로 검색된 DB 캐릭터를 기준으로 값을 초기화
    - 이후 View에서 관리 할 수 있게 VM에서 사용
    
    KazerothRaid
    DB 캐릭별 카제로스 레이드의 보스들을 정보를 제공
    - 보스들을 모아둬 관리 가능

--- app/src/main/java/com/hongmyeoun/goldcalc/model/goldCheck/KazerothRaid.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/model/goldCheck/KazerothRaid.kt
@@ -1,5 +1,7 @@
 package com.hongmyeoun.goldcalc.model.goldCheck
 
+import com.hongmyeoun.goldcalc.model.roomDB.Character
+
 enum class KazerothRaid(
     val boss: String,
     val seeMoreGold: Map<String, List<Int>>,
@@ -8,24 +10,68 @@ enum class KazerothRaid(
     ECHIDNA(
         boss = "에키드나",
         seeMoreGold = mapOf(
-            "normal" to listOf(2000, 3400),
+            "normal" to listOf(2200, 3400),
             "hard" to listOf(2800, 4100)
         ),
         clearGold = mapOf(
             "normal" to listOf(5000, 9500),
             "hard" to listOf(6000, 12500)
         )
-    ),
-}
+    );
 
-class KazerothRaidModel {
-    val echi = KazerothRaid.ECHIDNA.boss
-    var echiSMG = KazerothRaid.ECHIDNA.getBossInfo("normal").first + KazerothRaid.ECHIDNA.getBossInfo("hard").first
-    var echiCG =  KazerothRaid.ECHIDNA.getBossInfo("normal").second + KazerothRaid.ECHIDNA.getBossInfo("hard").second
-
-    private fun KazerothRaid.getBossInfo(mode: String): Pair<List<Int>, List<Int>> {
+    fun getBossInfo(mode: String): Pair<List<Int>, List<Int>> {
         val seeMoreGold = seeMoreGold[mode] ?: emptyList()
         val clearGold = clearGold[mode] ?: emptyList()
         return Pair(seeMoreGold, clearGold)
     }
 }
+
+class KazerothRaidModel(character: Character?) {
+    val echidna: Echidna = if (character != null) {
+        Echidna(character)
+    } else {
+        Echidna(null)
+    }
+}
+
+class Echidna(character: Character?) {
+    val name = KazerothRaid.ECHIDNA.boss
+    private val seeMoreGold = KazerothRaid.ECHIDNA.getBossInfo("normal").first + KazerothRaid.ECHIDNA.getBossInfo("hard").first
+    private val clearGold = KazerothRaid.ECHIDNA.getBossInfo("normal").second + KazerothRaid.ECHIDNA.getBossInfo("hard").second
+
+    private val getOnePhase = character?.checkList?.kazeroth?.get(0)?.phases?.get(0)
+
+    private val onePhaseDifficulty = getOnePhase?.difficulty?:"노말"
+    private val onePhaseIsClear = getOnePhase?.isClear?:false
+    private val onePhaseMCheck = getOnePhase?.mCheck?:false
+
+    val onePhase = PhaseInfo(
+        difficulty = onePhaseDifficulty,
+        isClearCheck = onePhaseIsClear,
+        moreCheck = onePhaseMCheck,
+        seeMoreGoldN = seeMoreGold[0],
+        seeMoreGoldH = seeMoreGold[2],
+        clearGoldN = clearGold[0],
+        clearGoldH = clearGold[2]
+    )
+
+    private val getTwoPhase = character?.checkList?.kazeroth?.get(0)?.phases?.get(1)
+
+    private val twoPhaseDifficulty = getTwoPhase?.difficulty?:"노말"
+    private val twoPhaseIsClear = getTwoPhase?.isClear?:false
+    private val twoPhaseMCheck = getTwoPhase?.mCheck?:false
+
+    val twoPhase = PhaseInfo(
+        difficulty = twoPhaseDifficulty,
+        isClearCheck = twoPhaseIsClear,
+        moreCheck = twoPhaseMCheck,
+        seeMoreGoldN = seeMoreGold[1],
+        seeMoreGoldH = seeMoreGold[3],
+        clearGoldN = clearGold[1],
+        clearGoldH = clearGold[3]
+    )
+
+}
+
+
+
