3ed53a4234d01364db478445110752192a9dd823
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-07 13:48:29 +0900

    ref : Character
    페이즈 구분을 할 수 없어서 구조 변경
    RaidList 내부에 Phase를 추가

--- app/src/main/java/com/hongmyeoun/goldcalc/model/roomDB/Character.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/model/roomDB/Character.kt
@@ -17,28 +17,66 @@ data class Character(
 
 data class CheckList(
     @ColumnInfo("Command") val command: List<RaidList> = listOf(
-        RaidList("Valtan"),
-        RaidList("Biackiss"),
-        RaidList("KoukuSaton"),
-        RaidList("Abrelshud"),
-        RaidList("Illiakan"),
-        RaidList("Kamen"),
+        RaidList(
+            name = "발탄",
+            phases = listOf(Phase(), Phase())
+        ),
+        RaidList(
+            name = "비아키스",
+            phases = listOf(Phase(), Phase())
+        ),
+        RaidList(
+            name = "쿠크세이튼",
+            phases = listOf(Phase(), Phase(), Phase())
+        ),
+        RaidList(
+            name = "아브렐슈드",
+            phases = listOf(Phase(), Phase(), Phase(), Phase())
+        ),
+        RaidList(
+            name = "일리아칸",
+            phases = listOf(Phase(), Phase(), Phase())
+        ),
+        RaidList(
+            name = "카멘",
+            phases = listOf(Phase(), Phase(), Phase(), Phase())
+        ),
     ),
     @ColumnInfo("AbyssDungeon") val abyssDungeon: List<RaidList> = listOf(
-        RaidList("Kayangel"),
-        RaidList("IvoryTower"),
+        RaidList(
+            name = "카양겔",
+            phases = listOf(Phase(), Phase(), Phase())
+        ),
+        RaidList(
+            name = "상아탑",
+            phases = listOf(Phase(), Phase(), Phase(), Phase())
+        ),
     ),
     @ColumnInfo("Kazeroth") val kazeroth: List<RaidList> = listOf(
-        RaidList("Echidna"),
+        RaidList(
+            name = "에키드나",
+            phases = listOf(Phase(), Phase())
+        )
     ),
     @ColumnInfo("Epic") val epic: List<RaidList> = listOf(
-        RaidList("Behemoth"),
+        RaidList(
+            name = "베히모스",
+            phases = listOf(Phase(), Phase())
+        )
     )
 )
 
 data class RaidList(
     val name: String,
-    @ColumnInfo("difficulty") val difficulty: String = "normal",
+    val phases: List<Phase> = listOf(
+        Phase(difficulty = "노말", isClear = false, mCheck = false)
+    ),
+    @ColumnInfo("isCheck") val isCheck: Boolean = false,
+
+)
+
+data class Phase(
+    @ColumnInfo("difficulty") val difficulty: String = "노말",
     @ColumnInfo("isClear") val isClear: Boolean = false,
     @ColumnInfo("mCheck") val mCheck: Boolean = false,
 )
