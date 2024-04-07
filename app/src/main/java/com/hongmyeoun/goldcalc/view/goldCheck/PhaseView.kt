ce2bb901bfc0be7b15e9c706cd3ccd4cd24519dc
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-07 13:31:07 +0900

    ref : PhaseView
    변경전
    - phase에서 난이도, 더보기체크와 같은 모든 변수 관리
    - twoPhaseBoss에서 받아온 클골과 더보기 골드를 전달
    - 하나 하나 값을 저장하기 힘듦
    
    변경후
    - twoPhaseBoss를 호출할때 phase에 넣어줄 값을 전부 전달
    - 계산을 VM에서 실행

--- app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/PhaseView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/PhaseView.kt
@@ -15,99 +15,86 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun phase(
-    gate: String,
-    seeMoreGoldN: Int,
-    seeMoreGoldH: Int,
-    clearGoldN: Int,
-    clearGoldH: Int,
-    onUpdateTotalGold: (Int) -> Unit
+fun PhaseUI(
+    phase: Int,
+    difficulty: String,
+    clearCheck: Boolean,
+    moreCheck: Boolean,
+    onLevelClicked: () -> Unit,
+    onClearClicked: (Boolean) -> Unit,
+    onMoreClicked: (Boolean) -> Unit
 ) {
-    var level by remember { mutableStateOf("노말") }
-    var seeMoreCheck by remember { mutableStateOf(false) }
-    var clearCheck by remember { mutableStateOf(false) }
-    var seeMoreGold by remember { mutableStateOf(0) }
-    var clearGold by remember { mutableStateOf(0) }
-
-    Column {
-        Row(
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .weight(3f)
+                .padding(start = 16.dp),
+            text = "$phase 관문"
+        )
+        OutlinedButton(
+            onClick = { onLevelClicked() }
         ) {
-            Text(
-                modifier = Modifier
-                    .weight(3f)
-                    .padding(start = 16.dp),
-                text = gate
-            )
-            OutlinedButton(
-                onClick = {
-                    level = if (level == "노말") "하드" else "노말"
-                    seeMoreGold = if (seeMoreCheck && level == "노말") seeMoreGoldN else if (seeMoreCheck) seeMoreGoldH else 0
-                    clearGold = if (clearCheck && level == "노말") clearGoldN else if (clearCheck) clearGoldH else 0
-                    onUpdateTotalGold(clearGold - seeMoreGold)
-                }
-            ) {
-                Text(text = level)
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Checkbox(
-                    checked = clearCheck,
-                    onCheckedChange = {
-                        clearCheck = !clearCheck
-                        clearGold = if (clearCheck && level == "노말") clearGoldN else if (clearCheck && level == "하드") clearGoldH else 0
-                        onUpdateTotalGold(clearGold - seeMoreGold)
-                    }
-                )
-                Text(text = "클리어")
-            }
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Checkbox(
-                    checked = seeMoreCheck,
-                    onCheckedChange = {
-                        seeMoreCheck = !seeMoreCheck
-                        seeMoreGold = if (seeMoreCheck && level == "노말") seeMoreGoldN else if (seeMoreCheck && level == "하드") seeMoreGoldH else 0
-                        onUpdateTotalGold(clearGold - seeMoreGold)
-                    }
-                )
-                Text(text = "더보기")
-            }
+            Text(text = difficulty)
         }
     }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = clearCheck,
+                onCheckedChange = { onClearClicked(it) }
+            )
+            Text(text = "클리어")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = moreCheck,
+                onCheckedChange = { onMoreClicked(it) }
+            )
+            Text(text = "더보기")
+        }
+    }
+
 }
 
 @Composable
 fun twoPhaseBoss(
     name: String,
-    seeMoreGold: List<Int>,
-    clearGold: List<Int>,
     totalGold: Int,
-    onUpdateTotalGoldOnePhase: (Int) -> Unit,
-    onUpdateTotalGoldTwoPhase: (Int) -> Unit
+
+    phaseOneLevel: String,
+    phaseOneSMC: Boolean,
+    phaseOneCC: Boolean,
+    onOnePhaseLevelClicked: () -> Unit,
+    onOnePhaseClearCheckBoxChecked: (Boolean) -> Unit,
+    onOnePhaseSeeMoreCheckBoxChecked: (Boolean) -> Unit,
+
+    phaseTwoLevel: String,
+    phaseTwoSMC: Boolean,
+    phaseTwoCC: Boolean,
+    onTwoPhaseLevelClicked: () -> Unit,
+    onTwoPhaseClearCheckBoxChecked: (Boolean) -> Unit,
+    onTwoPhaseSeeMoreCheckBoxChecked: (Boolean) -> Unit,
 ) {
     Column(
         modifier = Modifier.padding(16.dp)
@@ -132,169 +119,27 @@ fun twoPhaseBoss(
                 Text(text = "$totalGold")
             }
         }
-        phase(
-            gate = "1 관문",
-            seeMoreGoldN = seeMoreGold[0],
-            seeMoreGoldH = seeMoreGold[2],
-            clearGoldN = clearGold[0],
-            clearGoldH = clearGold[2],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldOnePhase(updated)
-            }
+
+        PhaseUI(
+            phase = 1,
+            difficulty = phaseOneLevel,
+            clearCheck = phaseOneCC,
+            moreCheck = phaseOneSMC,
+            onLevelClicked = { onOnePhaseLevelClicked() },
+            onClearClicked = { onOnePhaseClearCheckBoxChecked(it) },
+            onMoreClicked = { onOnePhaseSeeMoreCheckBoxChecked(it) }
         )
-        phase(
-            gate = "2 관문",
-            seeMoreGoldN = seeMoreGold[1],
-            seeMoreGoldH = seeMoreGold[3],
-            clearGoldN = clearGold[1],
-            clearGoldH = clearGold[3],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldTwoPhase(updated)
-            }
+
+        PhaseUI(
+            phase = 2,
+            difficulty = phaseTwoLevel,
+            clearCheck = phaseTwoCC,
+            moreCheck = phaseTwoSMC,
+            onLevelClicked = { onTwoPhaseLevelClicked() },
+            onClearClicked = { onTwoPhaseClearCheckBoxChecked(it) },
+            onMoreClicked = { onTwoPhaseSeeMoreCheckBoxChecked(it) }
         )
+
     }
 }
 
-
-@Composable
-fun threePhaseBoss(
-    name: String,
-    seeMoreGold: List<Int>,
-    clearGold: List<Int>,
-    totalGold: Int,
-    onUpdateTotalGoldOnePhase: (Int) -> Unit,
-    onUpdateTotalGoldTwoPhase: (Int) -> Unit,
-    onUpdateTotalGoldThreePhase: (Int) -> Unit,
-) {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "보스 이미지")
-            Text(
-                modifier = Modifier.weight(2f),
-                text = name
-            )
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "$totalGold")
-            }
-        }
-        phase(
-            gate = "1 관문",
-            seeMoreGoldN = seeMoreGold[0],
-            seeMoreGoldH = seeMoreGold[3],
-            clearGoldN = clearGold[0],
-            clearGoldH = clearGold[3],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldOnePhase(updated)
-            }
-        )
-        phase(
-            gate = "2 관문",
-            seeMoreGoldN = seeMoreGold[1],
-            seeMoreGoldH = seeMoreGold[4],
-            clearGoldN = clearGold[1],
-            clearGoldH = clearGold[4],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldTwoPhase(updated)
-            }
-        )
-        phase(
-            gate = "3 관문",
-            seeMoreGoldN = seeMoreGold[2],
-            seeMoreGoldH = seeMoreGold[5],
-            clearGoldN = clearGold[2],
-            clearGoldH = clearGold[5],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldThreePhase(updated)
-            }
-        )
-    }
-}
-
-@Composable
-fun fourPhaseBoss(
-    name: String,
-    seeMoreGold: List<Int>,
-    clearGold: List<Int>,
-    totalGold: Int,
-    onUpdateTotalGoldOnePhase: (Int) -> Unit,
-    onUpdateTotalGoldTwoPhase: (Int) -> Unit,
-    onUpdateTotalGoldThreePhase: (Int) -> Unit,
-    onUpdateTotalGoldFourPhase: (Int) -> Unit
-) {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "보스 이미지")
-            Text(
-                modifier = Modifier.weight(2f),
-                text = name
-            )
-            Row(
-                modifier = Modifier.weight(1f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                Icon(imageVector = Icons.Default.Favorite, contentDescription = "골드 이미지")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "$totalGold")
-            }
-        }
-        phase(
-            gate = "1 관문",
-            seeMoreGoldN = seeMoreGold[0],
-            seeMoreGoldH = seeMoreGold[4],
-            clearGoldN = clearGold[0],
-            clearGoldH = clearGold[4],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldOnePhase(updated)
-            }
-        )
-        phase(
-            gate = "2 관문",
-            seeMoreGoldN = seeMoreGold[1],
-            seeMoreGoldH = seeMoreGold[5],
-            clearGoldN = clearGold[1],
-            clearGoldH = clearGold[5],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldTwoPhase(updated)
-            }
-        )
-        phase(
-            gate = "3 관문",
-            seeMoreGoldN = seeMoreGold[2],
-            seeMoreGoldH = seeMoreGold[6],
-            clearGoldN = clearGold[2],
-            clearGoldH = clearGold[6],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldThreePhase(updated)
-            }
-        )
-        phase(
-            gate = "4 관문",
-            seeMoreGoldN = seeMoreGold[3],
-            seeMoreGoldH = seeMoreGold[7],
-            clearGoldN = clearGold[3],
-            clearGoldH = clearGold[7],
-            onUpdateTotalGold = { updated ->
-                onUpdateTotalGoldFourPhase(updated)
-            }
-        )
-    }
-}
