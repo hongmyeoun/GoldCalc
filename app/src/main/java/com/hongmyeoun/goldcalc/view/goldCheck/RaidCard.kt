20bf96d528582410a0a7bde885224e20b9b3044a
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-13 21:57:40 +0900

    ref : 필요없는 코드 삭제

--- app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/RaidCard.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/RaidCard.kt
@@ -54,37 +54,6 @@ fun RaidCard(
 
     }
 
-//    Row(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(vertical = 16.dp),
-//        verticalAlignment = Alignment.CenterVertically
-//    ) {
-//        Image(
-//            modifier = Modifier.weight(0.5f),
-//            painter = painterResource(id = raidImg),
-//            contentDescription = "던전 아이콘"
-//        )
-//        Text(
-//            modifier = Modifier.weight(1f),
-//            text = raidType
-//        )
-//        Row(
-//            modifier = Modifier.weight(1f),
-//            verticalAlignment = Alignment.CenterVertically,
-//            horizontalArrangement = Arrangement.End
-//        ) {
-//            Image(
-//                modifier = Modifier.size(25.dp),
-//                painter = painterResource(id = R.drawable.gold_coins),
-//                contentDescription = "골드 아이콘"
-//            )
-//            Spacer(modifier = Modifier.width(8.dp))
-//            Text(text = "$totalGold")
-//        }
-//
-//    }
-
     raidContent()
 
 }
