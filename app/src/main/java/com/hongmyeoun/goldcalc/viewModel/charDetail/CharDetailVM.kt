583424ea09e42463286e8dfce15f8bb64cd2cc9c
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-15 13:45:26 +0900

    feat : CharDetailVM 생성
    db 생성위치가 변경됨에 따라 CharacterDetailScreen에도 변화가 필요
    
    DI를 통해 repository를 주입받음

---
+++ app/src/main/java/com/hongmyeoun/goldcalc/viewModel/charDetail/CharDetailVM.kt
@@ -0,0 +1,34 @@
+package com.hongmyeoun.goldcalc.viewModel.charDetail
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hongmyeoun.goldcalc.model.roomDB.Character
+import com.hongmyeoun.goldcalc.model.roomDB.CharacterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class CharDetailVM @Inject constructor(
+    private val characterRepository: CharacterRepository
+): ViewModel() {
+    var isSaved by mutableStateOf(false)
+
+    fun isSavedName(charName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val savedNames = withContext(Dispatchers.IO) { characterRepository.getNames() }
+            isSaved = savedNames.contains(charName)
+        }
+    }
+
+    fun saveCharacter(character: Character) {
+        viewModelScope.launch(Dispatchers.IO) {
+            characterRepository.insertAll(character)
+        }
+    }
+}
