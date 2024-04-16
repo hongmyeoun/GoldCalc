06966c8f3f7127878aa5a96a4062c9177f6a68d9
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-16 17:24:49 +0900

    feat: 골드계산 기능 추가

--- app/src/main/java/com/hongmyeoun/goldcalc/viewModel/main/CharacterListVM.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/viewModel/main/CharacterListVM.kt
@@ -1,9 +1,10 @@
 package com.hongmyeoun.goldcalc.viewModel.main
 
+import androidx.compose.ui.util.fastSumBy
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hongmyeoun.goldcalc.model.roomDB.Character
-import com.hongmyeoun.goldcalc.model.roomDB.CharacterRepository
+import com.hongmyeoun.goldcalc.model.roomDB.character.Character
+import com.hongmyeoun.goldcalc.model.roomDB.character.CharacterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
@@ -14,13 +15,32 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CharacterListVM @Inject constructor(
-    private val characterRepository: CharacterRepository
+    val characterRepository: CharacterRepository,
 ): ViewModel() {
     val characters: StateFlow<List<Character>> = characterRepository.getAll().stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+
+
+
+    fun calcProgressPercentage(characterList: List<Character>): Float {
+        return if (calcWeeklyGold(characterList) != 0) calcEarnGold(characterList).toFloat() / calcWeeklyGold(characterList) else 0.0f
+    }
+
+    fun calcWeeklyGold(characterList: List<Character>): Int {
+        return characterList.fastSumBy { it.weeklyGold }
+    }
+
+    fun calcEarnGold(characterList: List<Character>): Int {
+        return characterList.fastSumBy { it.earnGold }
+    }
+
+    fun calcRemainGold(characterList: List<Character>): Int {
+        return calcWeeklyGold(characterList) - calcEarnGold(characterList)
+    }
 
     fun delete(character: Character) {
         viewModelScope.launch(Dispatchers.IO) {
             characterRepository.delete(character)
         }
     }
+
 }
