7e3d573481829367986bbac664c8b71acf699171
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-27 03:44:48 +0900

    ref: 필요없는 값 삭제

--- app/src/main/java/com/hongmyeoun/goldcalc/MainActivity.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -21,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -71,28 +75,6 @@ class MainActivity : ComponentActivity() {
                             if (isLoading) {
                                 LoadingScreen()
                             } else {
-//                                Column(
-//                                    modifier = modifier.verticalScroll(rememberScrollState()),
-//                                ) {
-//                                    characterList.forEach {
-//                                        val characterName = it.name
-//                                        val characterCardVM = remember { CharacterCardVM(characterRepository, characterName) }
-//
-//                                        val character by characterCardVM.character.collectAsState()
-//
-//                                        CharacterCard(
-//                                            navController = navController,
-//                                            viewModel = characterCardVM,
-//                                            onDelete = {
-//                                                characterListVM.delete(character)
-//                                                isLoading = true
-//                                            },
-//                                            onClick = { characterCardVM.enableDelay() }
-//                                        )
-//                                        Divider(modifier = Modifier.height(8.dp))
-//                                    }
-//                                }
-
                                 LazyColumn(modifier = modifier) {
                                     items(characterList, key = { item -> item.name }) {
                                         val characterName = it.name
@@ -159,12 +141,34 @@ fun LoadingScreen() {
     }
 }
 
+@Composable
+fun SimplephaseInfo(
+    isCheck: Boolean,
+    modifier: Modifier,
+    raidName: String,
+    phaseInfo: @Composable () -> Unit
+) {
+    if (isCheck) {
+        Column(modifier = modifier) {
+            Text(
+                text = raidName,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            phaseInfo()
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun CharacterPreview() {
 
 }
+
 
 
 //                            val pagerState = rememberPagerState(pageCount = { characterList.size })
