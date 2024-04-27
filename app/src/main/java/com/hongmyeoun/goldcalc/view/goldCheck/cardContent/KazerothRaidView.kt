7631c2b98110682dd729ab9fcaa10e9bdabe32b2
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-04-27 20:22:17 +0900

    ref: 함수이전에 check 확인

--- app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/cardContent/KazerothRaidView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/goldCheck/cardContent/KazerothRaidView.kt
@@ -44,55 +44,56 @@ fun KazerothRaid(viewModel: KazerothRaidVM) {
         }
     }
 
-    RaidCardUI(
-        bossImg = R.drawable.kazeroth_echidna,
-        isCheck = viewModel.echiCheck,
-        isRotated = roatated,
-        rotaR = rotaR,
-        onClick = { roatated = !roatated },
-        phaseCard = {
-            TwoPhaseBoss(
-                rotaR = rotaR,
+    if (viewModel.echiCheck) {
+        RaidCardUI(
+            bossImg = R.drawable.kazeroth_echidna,
+            isRotated = roatated,
+            rotaR = rotaR,
+            onClick = { roatated = !roatated },
+            phaseCard = {
+                TwoPhaseBoss(
+                    rotaR = rotaR,
 
-                name = viewModel.echidna.name,
-                raidBossImg = R.drawable.logo_echidna,
-                totalGold = viewModel.echidna.totalGold,
+                    name = viewModel.echidna.name,
+                    raidBossImg = R.drawable.logo_echidna,
+                    totalGold = viewModel.echidna.totalGold,
 
-                phaseOneLevel = viewModel.echidna.onePhase.level,
-                phaseOneGold = viewModel.echidna.onePhase.totalGold,
-                phaseOneSMC = viewModel.echidna.onePhase.seeMoreCheck,
-                phaseOneCC = viewModel.echidna.onePhase.clearCheck,
-                onOnePhaseLevelClicked = {
-                    viewModel.echidna.onePhase.onLevelClicked()
-                    viewModel.sumGold()
-                },
-                onOnePhaseClearCheckBoxChecked = {
-                    viewModel.echidna.onePhase.onClearCheckBoxClicked(it)
-                    viewModel.sumGold()
-                },
-                onOnePhaseSeeMoreCheckBoxChecked = {
-                    viewModel.echidna.onePhase.onSeeMoreCheckBoxClicked(it)
-                    viewModel.sumGold()
-                },
+                    phaseOneLevel = viewModel.echidna.onePhase.level,
+                    phaseOneGold = viewModel.echidna.onePhase.totalGold,
+                    phaseOneSMC = viewModel.echidna.onePhase.seeMoreCheck,
+                    phaseOneCC = viewModel.echidna.onePhase.clearCheck,
+                    onOnePhaseLevelClicked = {
+                        viewModel.echidna.onePhase.onLevelClicked()
+                        viewModel.sumGold()
+                    },
+                    onOnePhaseClearCheckBoxChecked = {
+                        viewModel.echidna.onePhase.onClearCheckBoxClicked(it)
+                        viewModel.sumGold()
+                    },
+                    onOnePhaseSeeMoreCheckBoxChecked = {
+                        viewModel.echidna.onePhase.onSeeMoreCheckBoxClicked(it)
+                        viewModel.sumGold()
+                    },
 
-                phaseTwoLevel = viewModel.echidna.twoPhase.level,
-                phaseTwoGold = viewModel.echidna.twoPhase.totalGold,
-                phaseTwoSMC = viewModel.echidna.twoPhase.seeMoreCheck,
-                phaseTwoCC = viewModel.echidna.twoPhase.clearCheck,
-                onTwoPhaseLevelClicked = {
-                    viewModel.echidna.twoPhase.onLevelClicked()
-                    viewModel.sumGold()
-                },
-                onTwoPhaseClearCheckBoxChecked = {
-                    viewModel.echidna.twoPhase.onClearCheckBoxClicked(it)
-                    viewModel.sumGold()
-                },
-                onTwoPhaseSeeMoreCheckBoxChecked = {
-                    viewModel.echidna.twoPhase.onSeeMoreCheckBoxClicked(it)
-                    viewModel.sumGold()
-                },
-            )
+                    phaseTwoLevel = viewModel.echidna.twoPhase.level,
+                    phaseTwoGold = viewModel.echidna.twoPhase.totalGold,
+                    phaseTwoSMC = viewModel.echidna.twoPhase.seeMoreCheck,
+                    phaseTwoCC = viewModel.echidna.twoPhase.clearCheck,
+                    onTwoPhaseLevelClicked = {
+                        viewModel.echidna.twoPhase.onLevelClicked()
+                        viewModel.sumGold()
+                    },
+                    onTwoPhaseClearCheckBoxChecked = {
+                        viewModel.echidna.twoPhase.onClearCheckBoxClicked(it)
+                        viewModel.sumGold()
+                    },
+                    onTwoPhaseSeeMoreCheckBoxChecked = {
+                        viewModel.echidna.twoPhase.onSeeMoreCheckBoxClicked(it)
+                        viewModel.sumGold()
+                    },
+                )
 
-        }
-    )
+            }
+        )
+    }
 }
