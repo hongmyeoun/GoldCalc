35c9820507ed6c2207cc3935bbc434a28310aefa
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-05-10 22:59:56 +0900

    feat: MainScreen에서 뒤로가기 버튼(navigationBar)을 두 번 눌렀을 때 앱 종료 기능 추가
    변경전
    - 시스템 종료 예고가 안뜸, 뒤로가기 연타하면 꺼짐
    
    변경후
    - Snackbar로 뒤로가기 버튼을 한번 누르면 알림이 뜨고, 그 상태에서 한 번 더 누르면 종료

--- app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainView.kt
@@ -1,12 +1,23 @@
 package com.hongmyeoun.goldcalc.view.main
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import com.hongmyeoun.goldcalc.viewModel.main.CharacterListVM
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(
@@ -14,8 +25,13 @@ fun MainScreen(
     navController: NavHostController,
     content: @Composable (Modifier) -> Unit
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    BackOnPressed(snackbarHostState)
+
     Scaffold(
-        topBar = { MainAppTopBar(navController, characterListVM) }
+        topBar = { MainAppTopBar(navController, characterListVM) },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) {
         content(
             Modifier
@@ -25,3 +41,23 @@ fun MainScreen(
     }
 }
 
+@Composable
+fun BackOnPressed(snackbarHostState: SnackbarHostState) {
+    val context = LocalContext.current
+    var backPressedState by remember { mutableStateOf(true) }
+    var backPressedTime = 0L
+    val scope = rememberCoroutineScope()
+
+    BackHandler(enabled = backPressedState) {
+        if(System.currentTimeMillis() - backPressedTime <= 1000L) {
+            // 앱 종료
+            (context as Activity).finish()
+        } else {
+            backPressedState = true
+            scope.launch {
+                snackbarHostState.showSnackbar("한 번 더 누르시면 앱이 종료됩니다.")
+            }
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+}
