9ddaaec7f2dd76526d31006fbabb0781e04bd059
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-05-14 21:40:45 +0900

    optimize

--- app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainView.kt
@@ -5,15 +5,11 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
