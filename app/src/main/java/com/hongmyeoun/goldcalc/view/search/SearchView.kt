6a2ab5b7e0cb35586d50c36ce96731b9c6bd1a09
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-05-22 20:58:48 +0900

    ref : SearchView와 VM으로 나누기

--- app/src/main/java/com/hongmyeoun/goldcalc/view/search/SearchView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/search/SearchView.kt
@@ -2,10 +2,8 @@ package com.hongmyeoun.goldcalc.view.search
 
 import android.content.Context
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,7 +18,6 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -34,10 +31,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,7 +45,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,28 +59,27 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.google.gson.GsonBuilder
 import com.hongmyeoun.goldcalc.R
 import com.hongmyeoun.goldcalc.model.lostArkApi.CharacterDetail
-import com.hongmyeoun.goldcalc.model.lostArkApi.CharacterInfo
 import com.hongmyeoun.goldcalc.model.lostArkApi.CharacterResourceMapper
 import com.hongmyeoun.goldcalc.model.lostArkApi.LostArkApiService
 import com.hongmyeoun.goldcalc.model.roomDB.character.Character
 import com.hongmyeoun.goldcalc.viewModel.charDetail.CharDetailVM
+import com.hongmyeoun.goldcalc.viewModel.search.SearchVM
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun CharacterScreen(navController: NavHostController) {
-    var characterName by remember { mutableStateOf("") }
-    var characterInfo by remember { mutableStateOf<List<CharacterInfo>>(emptyList()) }
-    var isLoading by remember { mutableStateOf(false) }
-    val isDark = isSystemInDarkTheme()
+fun CharacterScreen(navController: NavHostController, viewModel: SearchVM = viewModel()) {
+    val characterName by viewModel.characterName.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val characterList by viewModel.characterList.collectAsState()
+
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
 
     var isFocus by remember { mutableStateOf(false) }
     val keyboardController = LocalSoftwareKeyboardController.current
@@ -113,18 +108,14 @@ fun CharacterScreen(navController: NavHostController) {
                     isFocus = it.isFocused
                 },
             value = characterName,
-            onValueChange = { characterName = it },
+            onValueChange = { viewModel.onCharacterNameValueChange(it) },
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Done
             ),
             keyboardActions = KeyboardActions(
                 onDone = {
-                    isLoading = true
-                    // Retrofit을 사용하여 서버에서 데이터 가져오기
-                    scope.launch(Dispatchers.IO) {
-                        val info = getCharacter(context, characterName)
-                        characterInfo = info ?: emptyList()
-                        isLoading = false
+                    if (characterName.isNotEmpty()) {
+                        viewModel.onDone(context)
                     }
                     keyboardController?.hide()
                     focusState.clearFocus()
@@ -155,12 +146,8 @@ fun CharacterScreen(navController: NavHostController) {
                 {
                     IconButton(
                         onClick = {
-                            isLoading = true
-                            // Retrofit을 사용하여 서버에서 데이터 가져오기
-                            scope.launch(Dispatchers.IO) {
-                                val info = getCharacter(context, characterName)
-                                characterInfo = info ?: emptyList()
-                                isLoading = false
+                            if (characterName.isNotEmpty()) {
+                                viewModel.onDone(context)
                             }
                             keyboardController?.hide()
                             focusState.clearFocus()
@@ -188,7 +175,7 @@ fun CharacterScreen(navController: NavHostController) {
                     .fillMaxSize()
                     .padding(8.dp)
             ) {
-                items(characterInfo, key = { item -> item.characterName }) {
+                items(characterList, key = { item -> item.characterName }) {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -291,36 +278,6 @@ suspend fun getCharDetail(context: Context, characterName: String): CharacterDet
                 null
             }
         } catch (e: IOException) {
-            Log.d("실패", "네트워크 오류: $e")
-            null
-        }
-    }
-}
-
-suspend fun getCharacter(context: Context, characterName: String): List<CharacterInfo>? {
-    return withContext(Dispatchers.IO) {
-        val gson = GsonBuilder().setLenient().create()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(getString(context, R.string.lost_ark_url))
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(authorizationHeader(context))
-            .build()
-
-        val lostArkApiService = retrofit.create(LostArkApiService::class.java)
-
-        try {
-            val response = lostArkApiService.getCharacters(characterName).execute()
-            if (response.isSuccessful) {
-                var characterInfoList = response.body()
-                characterInfoList = characterInfoList?.sortedByDescending { it.itemMaxLevel }
-                characterInfoList
-            } else {
-                // 실패 처리
-                Log.d("실패", "서버 응답 실패: ${response.code()}")
-                null
-            }
-        } catch (e: IOException) {
-            // 네트워크 오류 처리
             Log.d("실패", "네트워크 오류: $e")
             null
         }
