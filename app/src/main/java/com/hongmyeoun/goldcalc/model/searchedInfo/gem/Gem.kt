d9890add270563eb583f4f3315460b0c3d649558
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-05-31 16:50:35 +0900

    move : 보석은 따로 관리해야하니 data class 분리
    작업 예정
    보석 정보 가져오기
    UI 배치

---
+++ app/src/main/java/com/hongmyeoun/goldcalc/model/searchedInfo/gem/Gem.kt
@@ -0,0 +1,9 @@
+package com.hongmyeoun.goldcalc.model.searchedInfo.gem
+
+data class Gem(
+    val type: String,
+    val level: Int,
+    val icon: String,
+    val skill: String,
+    val effect: String
+)
