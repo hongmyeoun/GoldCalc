30cdaf735a16edddaf5d585c54de8953e754d84d
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-06-19 19:03:58 +0900

    style : 어플 전체 색은 어둡게 가기위해 상단바 색 고정
    작업 완료
    상단바 색을 LightGray로 고정
    
    작업 예정
    어플전체
    상단 Statusbar 색을 배경과 동일
    하단 navigationBar 색도 배경과 동일
    -> 상하단바색을 변경하는게 아닌 뒷 배경의 색을 위로 끌어올릴 방법을 모름...
    
    로딩창
    로딩 로티 만들어보기
    
    검색
    서버별 원정대 나누기
    
    각인
    아르데타인의 기술은 effects 값이 없음
    
    캐릭터 상세
    viewModel에서 사용되는 공통 함수 model로 빼기
    
    장비
    빈값에 빈값인 이미지보여주기
    -> list에서 find로 찾고 없다면 빈값, 있다면 채우기 이런식?
    (나중에) 팔찌 상중하 옵션별로 색부여
    
    스킬
    트라이포드 이름이 길어지면 다음줄로 넘어가는데
    TextChip이랑 Spacer, Text가 같이 넘어가게 하기

--- app/src/main/java/com/hongmyeoun/goldcalc/ui/theme/Theme.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/ui/theme/Theme.kt
@@ -57,7 +57,7 @@ fun GoldCalcTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = LightGrayBG.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
