d14897e76c61f66d449cea99fe919ad875a0c2f0
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-06-26 00:44:01 +0900

    style : Main UI 전체 진행 한눈에 보기
    작업 완료
    전체 진행 한눈에 보기 색 통일
    
    작업 예정
    어플전체
    상단 Statusbar 색을 배경과 동일
    하단 navigationBar 색도 배경과 동일
    -> 상하단바색을 변경하는게 아닌 뒷 배경의 색을 위로 끌어올릴 방법을 모름...
    
    로딩창
    로딩 로티 만들어보기
    
    검색
    즐겨찾기 또는 검색기록 만들어 반복검색 하지 않기
    
    각인
    아르데타인의 기술은 effects 값이 없음
    
    캐릭터 상세
    viewModel에서 사용되는 공통 함수 model로 빼기
    
    장비
    빈값에 빈값인 이미지보여주기
    -> list에서 find로 찾고 없다면 빈값, 있다면 채우기 이런식?
    (나중에) 팔찌 상중하 옵션별로 색부여
    
    스킬
    트라이포드 이름이 길어지면 다음줄로 넘어가는데
    TextChip이랑 Spacer, Text가 같이 넘어가게 하기
    상세 배경이 검정 -> 고치기

--- app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainTopBar.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/main/MainTopBar.kt
@@ -3,14 +3,15 @@ package com.hongmyeoun.goldcalc.view.main
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -22,7 +23,6 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -53,10 +53,10 @@ import com.hongmyeoun.goldcalc.R
 import com.hongmyeoun.goldcalc.model.lostArkApi.CharacterResourceMapper
 import com.hongmyeoun.goldcalc.model.roomDB.character.Character
 import com.hongmyeoun.goldcalc.ui.theme.CharacterEmblemBG
-import com.hongmyeoun.goldcalc.ui.theme.DarkModeGray
+import com.hongmyeoun.goldcalc.ui.theme.ImageBG
 import com.hongmyeoun.goldcalc.ui.theme.LightGrayBG
-import com.hongmyeoun.goldcalc.ui.theme.MokokoGreen
 import com.hongmyeoun.goldcalc.view.characterDetail.normalTextStyle
+import com.hongmyeoun.goldcalc.view.characterDetail.titleTextStyle
 import com.hongmyeoun.goldcalc.viewModel.main.CharacterListVM
 import java.text.NumberFormat
 
@@ -112,10 +112,11 @@ fun MainAppProgressText(characterListVM: CharacterListVM) {
             Spacer(modifier = Modifier.width(4.dp))
 
             IconButton(
-                modifier = Modifier.size(12.dp),
+                modifier = Modifier.size(24.dp),
                 onClick = { characterListVM.onClicked() }
             ) {
                 Icon(
+                    modifier = Modifier.fillMaxSize(),
                     imageVector = Icons.Default.Info,
                     tint = Color.White,
                     contentDescription = "전체 진행사항 한눈에 보기"
@@ -148,17 +149,16 @@ fun MainAppProgressText(characterListVM: CharacterListVM) {
 }
 
 @Composable
-private fun ShowSimpleCurrent(characterListVM: CharacterListVM, isDark: Boolean = isSystemInDarkTheme()) {
+private fun ShowSimpleCurrent(characterListVM: CharacterListVM) {
     Dialog(onDismissRequest = { characterListVM.onDissmissRequest() }) {
-        val bgColor = if (isDark) DarkModeGray else Color.White
         val configuration = LocalConfiguration.current
         val screenHeight = configuration.screenHeightDp.dp
         val maxColumnHeight = (screenHeight * 0.8f) // 화면 높이의 80%
 
         Column(
             modifier = Modifier
-                .background(bgColor, RoundedCornerShape(16.dp))
-                .height(maxColumnHeight)
+                .background(ImageBG, RoundedCornerShape(16.dp))
+                .heightIn(max = maxColumnHeight)
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -174,8 +174,7 @@ private fun ShowSimpleCurrentTop() {
         modifier = Modifier.fillMaxWidth(),
         text = "숙제 현황",
         textAlign = TextAlign.Center,
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold
+        style = titleTextStyle(fontSize = 20.sp)
     )
     Spacer(modifier = Modifier.height(10.dp))
 }
@@ -193,135 +192,147 @@ private fun ShowSimpleCurrentContents(characterListVM: CharacterListVM) {
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 private fun SimpleTotal(character: Character) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = Modifier
+            .background(
+                color = LightGrayBG,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(8.dp),
     ) {
         Row(
-            modifier = Modifier.weight(2f),
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
-                modifier = Modifier
-                    .size(38.dp)
-                    .clip(RoundedCornerShape(36.dp))
-                    .background(CharacterEmblemBG),
-                contentAlignment = Alignment.Center
+            Row(
+                modifier = Modifier.weight(2f),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                GlideImage(
+                Box(
                     modifier = Modifier
-                        .size(35.dp)
-                        .padding(3.dp),
-                    contentScale = ContentScale.Crop,
-                    model = CharacterResourceMapper.getClassEmblem(character.className),
-                    contentDescription = "직업군"
-                )
+                        .size(38.dp)
+                        .clip(RoundedCornerShape(36.dp))
+                        .background(CharacterEmblemBG),
+                    contentAlignment = Alignment.Center
+                ) {
+                    GlideImage(
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(3.dp),
+                        contentScale = ContentScale.Crop,
+                        model = CharacterResourceMapper.getClassEmblem(character.className),
+                        contentDescription = "직업군"
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Column(
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = character.serverName,
+                        style = normalTextStyle()
+                    )
+
+                    Text(
+                        text = character.name,
+                        style = normalTextStyle(fontSize = 12.sp)
+                    )
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = character.className,
+                            style = normalTextStyle()
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(
+                            text = character.itemLevel,
+                            style = normalTextStyle()
+                        )
+                    }
+                }
+
+
             }
-
-            Spacer(modifier = Modifier.width(5.dp))
-
-            Column(
-                horizontalAlignment = Alignment.Start
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalArrangement = Arrangement.End
             ) {
-                Text(
-                    text = character.serverName,
-                    fontSize = 10.sp
-                )
-
-                Text(
-                    text = character.name,
-                    fontSize = 12.sp
-                )
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = character.className,
-                        fontSize = 10.sp
-                    )
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(
-                        text = character.itemLevel,
-                        fontSize = 10.sp
-                    )
+                Column {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        GlideImage(
+                            modifier = Modifier.size(24.dp),
+                            model = goldImage(character.weeklyGold),
+                            contentDescription = "골드"
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.End,
+                            text = character.weeklyGold.formatWithCommas(),
+                            style = normalTextStyle(fontSize = 14.sp)
+                        )
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        GlideImage(
+                            modifier = Modifier.size(24.dp),
+                            model = goldImage(character.earnGold),
+                            contentDescription = "골드"
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.End,
+                            text = character.earnGold.formatWithCommas(),
+                            style = normalTextStyle(fontSize = 12.sp)
+                        )
+                    }
                 }
             }
 
-
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            horizontalArrangement = Arrangement.End
-        ) {
-            Column {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    GlideImage(
-                        modifier = Modifier.size(18.dp),
-                        model = R.drawable.gold_coins,
-                        contentDescription = "골드"
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.End,
-                        text = character.weeklyGold.formatWithCommas(),
-                        fontSize = 16.sp
-                    )
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    GlideImage(
-                        modifier = Modifier.size(18.dp),
-                        model = R.drawable.gold_coin,
-                        contentDescription = "골드"
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.End,
-                        text = character.earnGold.formatWithCommas(),
-                        fontSize = 16.sp
-                    )
-                }
-            }
         }
 
+        Spacer(modifier = Modifier.height(6.dp))
+
+        val percentage = if (character.weeklyGold != 0) character.earnGold.toFloat() / character.weeklyGold else 0.0f
+        Box(contentAlignment = Alignment.Center) {
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .height(20.dp)
+                    .fillMaxWidth()
+                    .border(
+                        width = 0.5f.dp,
+                        color = Color.Gray,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                progress = percentage,
+                color = CharacterEmblemBG,
+                trackColor = Color.Transparent,
+                strokeCap = StrokeCap.Round
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterEnd)
+                    .padding(end = 8.dp),
+                text = percentage.toPercentage(),
+                textAlign = TextAlign.End,
+                style = normalTextStyle(fontSize = 12.sp)
+            )
+        }
     }
 
-    Spacer(modifier = Modifier.height(6.dp))
-
-    val percentage = if (character.weeklyGold != 0) character.earnGold.toFloat() / character.weeklyGold else 0.0f
-    Box(contentAlignment = Alignment.Center) {
-        LinearProgressIndicator(
-            modifier = Modifier
-                .height(20.dp)
-                .fillMaxWidth()
-                .border(1.dp, MokokoGreen),
-            progress = percentage,
-            color = MokokoGreen,
-            trackColor = Color.Transparent
-        )
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterEnd)
-                .padding(end = 8.dp),
-            text = percentage.toPercentage(),
-            textAlign = TextAlign.End,
-            fontSize = 12.sp
-        )
-    }
-
-    Spacer(modifier = Modifier.height(6.dp))
-    Divider()
-    Spacer(modifier = Modifier.height(6.dp))
+    Spacer(modifier = Modifier.height(16.dp))
 
 }
 
