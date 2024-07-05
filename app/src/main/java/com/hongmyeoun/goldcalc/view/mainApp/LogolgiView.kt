2dbc197163f70763695fea8fcfdd12e66ed04177
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-07-05 21:48:55 +0900

    feat : 시작 로고 추가
    작업 완료
    시작 로고 추가
    
    작업 예정
    메인
    LazyColumn 목록과 HorizontalPager로 변경 가능하게 하기
    
    장비
    빈값에 빈값인 이미지보여주기
    -> list에서 find로 찾고 없다면 빈값, 있다면 채우기 이런식?
    (나중에) 팔찌 상중하 옵션별로 색부여
    
    코드 전체
    하드 코딩된 String값 정리
    공통 함수 정리 및 viewModel 정리
    viewModel에서 사용되는 공통 함수 model로 빼기

--- app/src/main/java/com/hongmyeoun/goldcalc/view/mainApp/LogolgiView.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/mainApp/LogolgiView.kt
@@ -3,13 +3,21 @@ package com.hongmyeoun.goldcalc.view.mainApp
 import android.content.pm.ActivityInfo
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.hongmyeoun.goldcalc.model.roomDB.character.CharacterRepository
 import com.hongmyeoun.goldcalc.navigation.NavControl
 import com.hongmyeoun.goldcalc.ui.theme.GoldCalcTheme
+import com.hongmyeoun.goldcalc.view.common.LoadingScreen
+import kotlinx.coroutines.delay
 
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Composable
@@ -17,8 +25,24 @@ fun LoGolGi(characterRepository: CharacterRepository) {
     LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
     GoldCalcTheme {
+        var isLoading by remember { mutableStateOf(true) }
+
+        LaunchedEffect(Unit) {
+            delay(1500L)
+            isLoading = false
+        }
+
         Box(modifier = Modifier.safeDrawingPadding()) {
-            NavControl(characterRepository = characterRepository)
+            Crossfade(
+                targetState = isLoading,
+                label = "시작 로딩"
+            ) { loading ->
+                if (loading) {
+                    LoadingScreen()
+                } else {
+                    NavControl(characterRepository = characterRepository)
+                }
+            }
         }
     }
 }
