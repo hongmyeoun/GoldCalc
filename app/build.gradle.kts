e090403442e43804d23468c176d0af33795e434a
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-07-09 22:44:14 +0900

    version : 버그 픽스, 코드 구조 개선
    작업 완료
    버그 픽스 및 코드 구조 개선으로 버전 변경
    versionCode = 4
    versionName = "1.0.0-alpha-03"
    
    작업 예정
    메인
    LazyColumn 목록과 HorizontalPager로 변경 가능하게 하기
    
    장비
    빈값에 빈값인 이미지보여주기
    -> list에서 find로 찾고 없다면 빈값, 있다면 채우기 이런식?
    (나중에) 팔찌 상중하 옵션별로 색부여
    
    코드 전체
    공통 함수 정리 및 viewModel 정리
    viewModel에서 사용되는 공통 함수 model로 빼기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0-alpha-02"
+        versionCode = 4
+        versionName = "1.0.0-alpha-03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
