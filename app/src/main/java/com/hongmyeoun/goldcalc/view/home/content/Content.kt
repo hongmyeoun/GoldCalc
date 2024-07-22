c91f91989e73ff16d0ff41a070abf30e029ac088
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-07-22 21:16:06 +0900

    feat : 숙제 캐릭 추가 아이콘
    작업 완료
    목록형, 이미지형에서 추가 플로팅 아이콘을 만듦
    
    작업 예정
    캐릭 추가 애니메이션으로 검색추가, 그냥추가 만들기

--- app/src/main/java/com/hongmyeoun/goldcalc/view/home/content/Content.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/view/home/content/Content.kt
@@ -1,10 +1,24 @@
 package com.hongmyeoun.goldcalc.view.home.content
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.hongmyeoun.goldcalc.model.roomDB.character.Character
 import com.hongmyeoun.goldcalc.model.roomDB.character.CharacterRepository
+import com.hongmyeoun.goldcalc.ui.theme.ImageBG
 
 @Composable
 fun HomeContent(
@@ -31,5 +45,25 @@ fun HomeContent(
             navController = navController,
             isLoading = isLoading,
         )
+    }
+
+    val floatingPadding = if (isListView) Modifier.padding(bottom = 32.dp, end = 32.dp) else Modifier.padding(start = 32.dp, end = 32.dp, bottom = 48.dp)
+
+    Box(
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+            .then(floatingPadding),
+        contentAlignment = if (isListView) Alignment.BottomEnd else Alignment.BottomCenter
+    ) {
+        FloatingActionButton(
+            modifier = Modifier.size(48.dp),
+            onClick = { /*TODO*/ },
+            shape = CircleShape,
+            containerColor = Color.White,
+            contentColor = ImageBG
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
+        }
     }
 }
