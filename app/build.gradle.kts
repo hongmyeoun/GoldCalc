14f1690c2be0e0566464c4b678ad76d4440e8eb2
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2025-01-08 11:00:42 +0900

    version: 환수사 업데이트 버전업
    추가 사항
    versionCode = 26
    versionName = "1.2.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 34
-        versionCode = 25
-        versionName = "1.2.0"
+        versionCode = 26
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
