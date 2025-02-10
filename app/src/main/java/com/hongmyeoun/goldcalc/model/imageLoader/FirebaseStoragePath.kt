47c57472d4aeabf38f5164e4fd9948b696309888
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2025-02-10 12:38:56 +0900

    feat: 레이드 로고 루트 추가
    변경 사항
    - 로고 루트를 추가

--- app/src/main/java/com/hongmyeoun/goldcalc/model/imageLoader/FirebaseStoragePath.kt
+++ app/src/main/java/com/hongmyeoun/goldcalc/model/imageLoader/FirebaseStoragePath.kt
@@ -2,7 +2,8 @@ package com.hongmyeoun.goldcalc.model.imageLoader
 
 class FirebaseStoragePath {
     object RaidImage {
-        const val ROOT = "raid_main_images/"
+        const val MAIN_ROUTE = "raid_main_images/"
+        const val LOGO_ROUTE = "raid_logo_images/"
 
         const val COMMAND = "command/"
         const val ABYSS_DUNGEON = "abyss_dungeon/"
