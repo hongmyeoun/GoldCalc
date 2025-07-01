d6de0cc6b4d3b43ed6c699872649d098fd22e871
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2025-07-01 14:03:31 +0900

    version: 골드 수정
    변경 사항
    - 노말과 싱글에 골드가 차이가 없지만 추가
    - 하드 클골 및 더보기 골드 수정
    
    versionCode = 30
    versionName = "1.3.1"
    
    해야할 일
    - 노말 모드와 싱글모드의 골드가 같아 UI 리로드가 안되는 현상 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.3.0"
+        versionCode = 30
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
