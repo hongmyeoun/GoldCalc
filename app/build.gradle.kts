529b57132ac2b930a5b9b25cbd8635c69f853cb6
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2025-07-22 10:59:27 +0900

    version: 노말 모드와 싱글모드 골드 문제 해결
    변경사항
    - 노말 모드와 싱글모드의 골드가 같아 UI 리로드가 안되는 현상 수정
    
    versionCode = 32
    versionName = "1.3.3"
    
    해야할 일
    - 레이드 보상 기재 및 총 재료 표기탭 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "1.3.2"
+        versionCode = 32
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
