a79e1beeeaf8ef6155b0e18651d77e25533d0afa
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2026-03-31 16:20:55 +0900

    version: 버그 수정
    변경사항
    versionCode = 38
    versionName = "1.4.3"
    - 세르카 토탈 골드가 변경되던 문제 해결
    
    해야할 일
    - 귀속 골드 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "1.4.2"
+        versionCode = 38
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
