1b0e0c87301111021293ae4fa40b275546027d90
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2026-08-05 11:03:30 +0900

    version: 벨가르딘 업데이트
    변경사항
    - 벨가르딘 추가
    
    versionCode = 43
    versionName = "1.4.8"
    
    추가할것
    - 완갑 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.hongmyeoun.goldcalc"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.4.7"
+        versionCode = 43
+        versionName = "1.4.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
